1b8e9183d6e2f636ee6693677ed5e4c8d331209f
Tim Stewart <tim.j.stewart@gmail.com>
2021-11-06 12:20:58 -0600

    Merging results to fix overwrite bug.

--- main.py
+++ main.py
@@ -22,8 +22,28 @@ class LintResult:
 LinterResults = Dict[Path, List[LintResult]]
 
 
+def merge_results(lhs: LinterResults,
+                  rhs: LinterResults) -> LinterResults:
+    results: LinterResults = defaultdict(list)
+    for k, v in lhs.items():
+        results[k].extend(v)
+    for k, v in rhs.items():
+        results[k].extend(v)
+    return results
+
+
 class Error(LintResult):
-    pass
+
+    def __init__(self,
+                 path: Path,
+                 lintable: Optional['Lintable'],
+                 error: str
+                 ) -> None:
+        super().__init__(path, lintable)
+        self.error = error
+
+    def __str__(self):
+        return f"{self.__class__.__name__} in: {self.path}: {self.error}"
 
 
 class Warning(LintResult):
@@ -37,7 +57,7 @@ class Warning(LintResult):
         self.warning = warning
 
     def __str__(self):
-        return f"{self.__class__.__name__}: {self.path}, '{self.warning}'"
+        return f"{self.__class__.__name__} in: {self.path}: {self.warning}"
 
 
 class Skipped(LintResult):
@@ -49,8 +69,12 @@ class LintContext:
     def __init__(self, cwd: Path) -> None:
         self.cwd = cwd
 
-    def with_cwd(self, new_cwd: Path) -> 'LintContext':
-        return LintContext(new_cwd)
+    def cd(self, directory: str) -> Optional['LintContext']:
+        new_dir = Path(self.cwd / directory)
+        if not new_dir.is_dir():
+            return None
+        else:
+            return LintContext(new_dir)
 
 
 class Lintable(ABC):
@@ -70,9 +94,17 @@ class Directory(Lintable):
         self.children = list(children) if children else []
 
     def lint(self, context: LintContext) -> LinterResults:
-        results = {}
+        results: LinterResults = defaultdict(list)
+        my_context = context.cd(self.path)
+        if not my_context:
+            if not self.optional:
+                results[context.cwd].append(Error(
+                    context.cwd, self,
+                    f"required directory '{self.path}' does not exist"))
+            return results
+
         for child in self.children:
-            results.update(child.lint(context))
+            results = merge_results(results, child.lint(my_context))
         return results
 
 
@@ -113,13 +145,14 @@ class Linter:
     def run(self, root: Path) -> LinterResults:
         results: LinterResults = defaultdict(list)
 
-        linted_map: Dict[Path, bool] = {entry: False
-                                       for entry in root.iterdir()}
+        linted_map: Dict[Path, bool] = {
+            entry: False for entry in root.iterdir()}
 
         # Lint selected files and directories
+        context = LintContext(root)
         for child in self.children:
-            lint_results = child.lint(self)
-            results.update(lint_results)
+            lint_results = child.lint(context)
+            results = merge_results(results, lint_results)
             for result in lint_results:
                 linted_map[result] = True
 
@@ -128,7 +161,8 @@ class Linter:
             for fso, linted in linted_map.items():
                 if not linted:
                     fso_type = "directory" if fso.is_dir() else "file"
-                    results[root].append(Warning(fso, None, f"unexpected {fso_type}"))
+                    results[root].append(
+                        Warning(fso, None, f"unexpected {fso_type}"))
 
         return results
 
@@ -155,14 +189,16 @@ def file(**kwargs):
 
 def main():
     linter = define_linter(
-        strict_directory_contents=True,
+        strict_directory_contents=False,
         children=[
             file(path="requirements.txt"),
             directory(path="venv", optional=False),
-            directory(path="sample_data", optional=False,
-                      children=[files(glob="*.json")]),
+            directory(path="sample_data", optional=False, children=[
+                files(glob="*.json")
+            ]),
             directory(path="optional", optional=True),
-            directory(path="must"),
+            directory(path="must", optional=False),
+            directory(path=".gitr", optional=False),
             directories(glob="subdir_*", min=1),
             files(glob="logfile.*.log")
         ])
